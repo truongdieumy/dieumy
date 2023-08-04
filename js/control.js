b028cd709d730c9e2a79d34cc996fcde458406f0
truongdieumy <141378330+truongdieumy@users.noreply.github.com>
2023-08-04 08:12:31 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+     strings: ["Web Designer.", "Code Developer.","Apps Designer."],
+       typeSpeed:50,
+        backSpeed:50,
+       loop:true,
+        });
